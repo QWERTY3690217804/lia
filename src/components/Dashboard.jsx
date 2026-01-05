8b22b631f91533bc8552ca5f4bff96293d1d665a
qwerty3690217804 <qwerty3690217804@gmail.com>
2026-01-05 00:40:20 -0800

    updated images

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,17 +2,38 @@ import React from 'react';
 import {Outlet} from 'react-router-dom';
 import './Dashboard.css';
 import giraffePre from '../assets/giraffePre.png';
-import cow from '../assets/cow.png';
-import bear from '../assets/bear.png'
+import wh from '../assets/wh.png';
+import pronouns from '../assets/pronouns.png';
 import title from '../assets/title.png';
 import subtitle from '../assets/subtitle.png'
 import intro from '../assets/intro.png'
 import Play from './Play';
+
+const images = [
+    { src: wh, id: 1},
+    { src: pronouns, id: 2},
+    { src: cow, id: 3},
+    { src: bear, id: 4},
+    { src: cow, id: 5}
+];
+
 function Dashboard() {
+    const handleClick = (id) => {
+        console.log('Clicked: ', id)
+    }
     return (
         <div>
-            <Outlet />
-            <img src={giraffePre} width={900}/>    
+            <img src={giraffePre} width={900}/>  
+            <div className="grid-container">
+                {images.map(({src, id}) => (
+                    <button
+                        key={id}
+                        className="grid-item"
+                        onClick={() => handleClick(id)}>
+                            <img src={src}/>
+                    </button>
+                ))}
+            </div>  
         </div>
     )
 }
